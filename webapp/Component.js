de17ee9c9c2865dc224946e3dc5c0b7892c12b4f
CaioFragoso23 <caiocesarfragoso@gmail.com>
2026-04-27 13:20:43 +0000

    feat/Uploading captreinologum

---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("sd.logum.com.captreinologum.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
